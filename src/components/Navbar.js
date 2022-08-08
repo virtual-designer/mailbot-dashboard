caecd8fdaf821c5bf203e5dd00c69293e3cd74c4
Ar Rakin <rakinar2@gmail.com>
2022-08-09 00:07:52 +0600

    feat: finishing touch of navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,31 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 import Button from './Button';
-import { BiArrowBack } from 'react-icons/bi';
+import { BiArrowBack, BiMenu } from 'react-icons/bi';
 
 export default class Navbar extends React.Component {
+    state = {
+        isOpen: false
+    };
+
+    toggle() {
+        this.setState({ isOpen: !this.state.isOpen });
+    }
+
     render() {
         return (
-            <nav className="p-3 flex items-center navbar">
+            <nav className={"p-3 flex items-center navbar" + (!this.state.isOpen ? " closed" : "")}>
+                <div className="navbarToggler mobile" onClick={() => this.toggle()}>
+                    <BiMenu size="25px" />
+                </div>
                 <Link to="/" className="text-2xl pr-4">MailBot</Link>
-                <ul>
-                    <li className="mobile toggler"><BiArrowBack size="25px" /></li>
+                <div className="mobile"></div>
+                <ul className="z-10">
+                    <li className="toggler mobile" onClick={() => this.toggle()}><BiArrowBack className="icon mobile" size="25px" /></li>
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/contact">Contact</Link></li>
                 </ul>
+                <div className="overlay mobile fixed top-0 left-0 h-[100vh] w-[100vw] z-[3]" onClick={() => this.toggle()}></div>
                 <div className="absolute top-3 right-3 desktop">
                     <Button>Login</Button>
                 </div>
