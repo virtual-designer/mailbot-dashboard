b1eea0f44d9d4b0c18eaf64626556b981f89e067
Ar Rakin <rakinar2@gmail.com>
2022-08-11 17:32:44 +0600

    feat: add login page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
+import Login from './pages/Login';
 import Navbar from './components/Navbar';
 
 export default function App() {
@@ -10,6 +11,8 @@ export default function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/contact" element={<Contact />} />
+
+                <Route path="/login" element={<Login />} />
             </Routes>
         </div>
     );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,35 @@
-export default function Login() {
-    return (
-        <div>
-            <h1>Login Page</h1>
-        </div>
-    );
+import React, { useState } from 'react';
+import Button from '../components/Button';
+
+export default class Login extends React.Component {
+    state = {
+        forgotPassword: false
+    };
+
+    render() {
+        return (
+            <div className='bg-[#f2f2f2] h-[93vh]'>
+                <div className="login">
+                    <form method="post" className='bg-white mt-2 md:m-5 p-4 rounded shadow'>
+                        <h1 className="text-3xl md:text-4xl mb-8 text-center w-[75vw] md:w-[14.5vw] mx-0 px-0">Login</h1>
+                        <div className="form-group">
+                            <label htmlFor="username">Username</label>
+                            <input type="text" id="username" name="username" className="form-control w-[75vw] md:w-[14.5vw]" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" id="password" name="password" className="form-control w-[75vw] md:w-[14.5vw]" />
+                            <a className="text-xs hover:underline" href='#' onClick={() => this.setState({ forgotPassword: !this.state.forgotPassword })}>Forgot Password?</a>
+                            {this.state.forgotPassword && (<div className='bg-gray-100 p-2 rounded my-2 break-words text-sm w-[75vw] md:w-[14.5vw]'>
+                                Please contact <a href="mailto:mailbot@onesoftnet.eu.org">mailbot@onesoftnet.eu.org</a> or <span className="text-gray-400">rakinar2#7578</span> on Discord including your username. The password reset link will be sent back to you.
+                            </div>)}
+                        </div>
+                        <div className="form-group">
+                            <Button>Submit</Button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        );
+    }
 }
